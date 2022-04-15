32367605285bbae10a1047d89e366a54efeed618
Oscar Triano (dotoscat) <dotoscat@gmail.com>
2022-04-15 20:50:09 +0200

    Add LICENSE

--- src/game.hpp
+++ src/game.hpp
@@ -10,6 +10,20 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+// Copyright 2022 Oscar Triano García
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
 #include "event_manager.hpp"
 #include "current_piece.hpp"
 #include "abuse_negation.hpp"
